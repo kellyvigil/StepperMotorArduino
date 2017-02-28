ffe2b8c0057690f8e21532e621fe7ddec05ed3e4
Kelly <programmingstation05@Programming-Station-05-iMac-2.local>
2017-02-28 18:59:42 -0500

    pull some unnecessary things out

--- StepperMotor/StepperMotor.ino
+++ StepperMotor/StepperMotor.ino
@@ -1,16 +1,4 @@
-/*
- * Microstepping demo
- *
- * This requires that M0, M1 be connected in addition to STEP,DIR
- *
- * Copyright (C)2015 Laurentiu Badea
- *
- * This file may be redistributed under the terms of the MIT license.
- * A copy of this license has been included with this distribution in the file LICENSE.
- */
 #include <Arduino.h>
-#include "DRV8834.h"
-#include "A4988.h"
 #include "DRV8825.h"
 
 // Motor steps per revolution. Most steppers are 200 steps or 1.8 degrees/step
@@ -19,65 +7,35 @@
 // All the wires needed for full functionality
 #define DIR 8
 #define STEP 9
-#define ENBL 10
-
-// 2-wire basic config, microstepping is hardwired on the driver
-// BasicStepperDriver stepper(DIR, STEP);
-
-// microstep control for DRV8834
-//#define M0 10
-//#define M1 11
-//DRV8834 stepper(MOTOR_STEPS, DIR, STEP, M0, M1);
-// DRV8834 stepper(MOTOR_STEPS, DIR, STEP, ENBL, M0, M1);
-
-// microstep control for A4988
-// #define MS1 10
-// #define MS2 11
-// #define MS3 12
-// A4988 stepper(MOTOR_STEPS, DIR, STEP, MS1, MS2, MS3);
+#define ENBL 13
 
 // microstep control for DRV8825
 // same pinout as A4988, different pin names, supports 32 microsteps
- #define MODE0 10
- #define MODE1 11
- #define MODE2 12
- DRV8825 stepper(MOTOR_STEPS, DIR, STEP, MODE0, MODE1, MODE2);
+#define MODE0 10
+#define MODE1 11
+#define MODE2 12
+DRV8825 stepper(MOTOR_STEPS, DIR, STEP, MODE0, MODE1, MODE2);
+
+
+/*
+* Microstepping mode: 1,2,4,8,16 or 32(DRV8834 only)
+* Mode 1 is full speed.
+* Mode 32 is 32 microsteps per step.
+* The motor should rotate just as fast (set RPM),
+* but movement precision is increased.
+*/
+//MicroStep speed devider
+int msSpeed = 8;
+int RPM = 3;
 
 void setup() {
-    /*
-     * Set target motor RPM.
-     * These motors can do up to about 200rpm.
-     * Too high will result in a high pitched whine and the motor does not move.
-     */
-    stepper.setRPM(30);
+  stepper.setRPM(RPM);
 }
 
 void loop() {
-    /*
-     * Microstepping mode: 1,2,4,8,16 or 32(DRV8834 only)
-     * Mode 1 is full speed.
-     * Mode 32 is 32 microsteps per step.
-     * The motor should rotate just as fast (set RPM),
-     * but movement precision is increased.
-     */
-    stepper.setMicrostep(4);
+  
+  stepper.setMicrostep(msSpeed);
+  
+  stepper.move(800 * msSpeed);
 
-    // 180 degrees now takes 100 * 8 microsteps
-    for(int i=1; i<100; i++){
-    
-    stepper.move(i*4);
-    
-    delay(10);
-    
-    stepper.move(-i*4);
-    delay(10);
-    
-    }
-    
-
-   // delay(100);
-    
-  //  stepper.move(-200*4);
-    
-  //  delay(100);
 }
